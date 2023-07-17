7ee8418078cd61b82a71d6fc745c9654560042f1
natiBus <natimogli@gmail.com>
2023-07-17 21:52:48 +0200

    init firebase

---
+++ src/firebaseStuff.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAVvffY6majTe77N7Qo6qXbT7zivafc2F4",
+  authDomain: "final-fullstack-demo.firebaseapp.com",
+  projectId: "final-fullstack-demo",
+  storageBucket: "final-fullstack-demo.appspot.com",
+  messagingSenderId: "871244700828",
+  appId: "1:871244700828:web:8a36549a52184995992159"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
